39e88e4b612dd7c8b13bcccadf855968dea4af74
Catrine82 <fateeva-ea@serviceit.su>
2022-09-17 20:07:46 +0300

    example001-seminar 1

---
+++ example001/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+//Задача 1
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int kvadrat = number * number;
+Console.WriteLine($"Quadr of {number} is {kvadrat}");
+*/
+/*
+//Задача 2
+Console.Write("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"{num1} is a quad of {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} is not a quad of {num2}");
+}
+*/
+/*
+//Задача 3
+Console.Write("Input positive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = num * -1;
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+*/
+/*
+//Задача 4
+Console.Write("Input three-digit integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 10;
+Console.WriteLine($"Last digit {num} is {result}");
+*/
